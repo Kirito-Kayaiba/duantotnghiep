8f5e160fc9ad236bea256f761420fba1c3118923
Kirito-Kayaiba <luanlkps21807@fpt.edu.vn>
2023-10-21 20:28:57 +0700

    update reset password

--- app/Http/Controllers/SanphamController.php
+++ app/Http/Controllers/SanphamController.php
@@ -3,21 +3,11 @@
 namespace App\Http\Controllers;
 use App\Models\SanPham;
 use App\Models\Loai;
+
 use Illuminate\Http\Request;
 
 class SanPhamController extends Controller
 {
-    public function index(){
-        $sp= \DB::table('sanpham')->limit(10)->get();
-        $sp2= \DB::table('sanpham')->where('id_san_pham','>=',100)->limit(20)->get();
-        $sp3= \DB::table('sanpham')->where('id_san_pham','>=',30)->limit(20)->get();
-        $sp4= \DB::table('sanpham')->where('id_san_pham','>=',130)->limit(20)->get();
-        return view('home',['sp'=>$sp,'sp2'=>$sp2,'sp3'=>$sp3,'sp4'=>$sp4]);
-    }
-    public function dt(){
-        $dt= \DB::table('sanpham')->where('id_loai','1')->get();
-        return view('shop',['dt'=>$dt]);
-    }
     public function danhSach(){
         $sanpham = SanPham::orderBy('id_san_pham','DESC')->get();
         return view('admin.sanpham.danhsach',['sanpham'=>$sanpham]);
