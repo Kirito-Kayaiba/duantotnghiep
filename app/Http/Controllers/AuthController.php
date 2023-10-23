414a46359900e41d8295db961bcead9b9e705d69
HoVanDat <hodat1204@gmail.com>
2023-10-23 10:10:03 +0700

    ok

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Models\User;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Hash;
 
 
